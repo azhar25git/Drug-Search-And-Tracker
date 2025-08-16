e3aa93ffb13e03e88128bce6199d118de7f48bc2
Azhar Uddin <16219627+azhar25git@users.noreply.github.com>
2025-08-17 02:08:25 +0600

    added: testing for user drugs and search

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -10,20 +10,36 @@ class ApiTest extends TestCase
 {
     use RefreshDatabase;
 
-    public string $prefix = '/api/v1';
+    private string $prefix = '/api/v1';
 
-    public function test_register()
+    private User $user;
+
+    private string $token = '';
+
+    private function getToken()
+    {
+        if ($this->token) {
+            return $this->token;
+        }
+
+        $user = User::factory()->create();
+        $this->token = $user->createToken('test')->plainTextToken;
+
+        return $this->token;
+    }
+
+    public function test_register_success()
     {
         $response = $this->postJson("{$this->prefix}/register", [
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'password'
+            'password' => 'password',
         ]);
 
         $response->assertStatus(201)->assertJsonStructure(['token']);
     }
 
-    public function test_login()
+    public function test_login_success()
     {
         $user = User::factory()->create(['password' => bcrypt('password')]);
 
@@ -35,4 +51,95 @@ class ApiTest extends TestCase
         $response->assertStatus(200)->assertJsonStructure(['token']);
     }
 
+    public function test_search_with_results()
+    {
+        $response = $this->getJson("{$this->prefix}/search?drug_name=paracetamol");
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                '*' => [
+                    'rxcui',
+                    'name',
+                    'ingredient_base_names',
+                    'dosage_forms',
+                ],
+            ]);
+    }
+
+    public function test_search_returns_empty()
+    {
+        $response = $this->getJson("{$this->prefix}/search?drug_name=paracetamols");
+
+        $response->assertStatus(200)
+            ->assertExactJson([]);
+    }
+
+    public function test_add_drug_success()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068'])
+            ->assertStatus(201);
+    }
+
+    public function test_add_drug_already_added_for_user()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068']);
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068'])
+            ->assertStatus(400);
+    }
+
+    public function test_add_drug_not_found()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068a'])
+            ->assertStatus(422);
+    }
+
+    public function test_get_user_drug_list()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068']);
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->getJson("{$this->prefix}/drugs")
+            ->assertStatus(200)
+            ->assertJsonIsArray();
+    }
+
+    public function test_delete_user_drug_success()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->postJson("{$this->prefix}/drugs", ['rxcui' => '308068']);
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->deleteJson("{$this->prefix}/drugs/308068")
+            ->assertStatus(200);
+    }
+    public function test_delete_user_drug_not_found()
+    {
+
+        $token = $this->getToken();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->deleteJson("{$this->prefix}/drugs/308068")
+            ->assertStatus(404);
+    }
 }
